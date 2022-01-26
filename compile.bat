3ad91a8673a5c5a341d055b74a00092c3e755e3f
Yuri Andriaccio <yurand2000@gmail.com>
2022-01-26 13:20:32 +0100

    More Fixes

--- compile.bat
+++ compile.bat
@@ -1,12 +1,11 @@
 @echo off
 
-
 echo Compiling
-call javac -d bin -cp @compile_data\libs_jar.txt @compile_data\sources.txt
+call javac -d bin -cp bin @compile_data\sources.txt
 
 echo Making Jars
 cd bin
-jar cfe WinsomeServer.jar winsome.server_app.ServerAppMain @..\compile_data\classes_server.txt @com_libs.txt
-jar cfe WinsomeClient.jar winsome.console_app.ConsoleAppMain @..\compile_data\classes_client.txt @com_libs.txt
+jar cfe WinsomeServer.jar winsome.server_app.ServerAppMain @..\compile_data\classes_server.txt @..\compile_data\com_libs.txt
+jar cfe WinsomeClient.jar winsome.console_app.ConsoleAppMain @..\compile_data\classes_client.txt @..\compile_data\com_libs.txt
 cd ..
 pause
